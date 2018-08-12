53ba147524c5f113014f761323a891223348239e
Maxim Zviagintsev <maxim.zviagintsev@gmail.com>
2018-08-12 16:32:32 +0300

    Passport JWT auth strategy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI:
-        'mongodb+srv://mz-admin:fdLlfi52Pf987Rujnfw897Kkf@clustermz-cvkpd.mongodb.net/test?retryWrites=true'
+    mongoURI: 'mongodb+srv://mz-admin:fdLlfi52Pf987Rujnfw897Kkf@clustermz-cvkpd.mongodb.net/test?retryWrites=true',
+    secretOrKey: 'secret'
 };
