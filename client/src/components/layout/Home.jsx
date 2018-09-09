4a25365289ed0c22a1726a4ed1db26a36ccae7bd
Maxim Zviagintsev <maxim.zviagintsev@gmail.com>
2018-09-09 12:03:23 +0300

    Home component restyled

--- client/src/components/layout/Home.jsx
+++ client/src/components/layout/Home.jsx
@@ -17,19 +17,19 @@ class Home extends Component {
             <Container fluid className={classes}>
                 <Grid className="home__inner">
                     <Grid.Row centered columns={1}>
-                        <Header as='h1' textAlign='center' className="header__custom">React Network</Header>
+                        <Header as='h1' textAlign='center'  color='orange' className="header__custom">React Network</Header>
                         <Header as='h3' textAlign='center' className="header__custom">Create a profile/portfolio, share
                             posts and get help from other developers</Header>
                     </Grid.Row>
                     <Grid.Row columns={2}>
                         <Grid.Column width={8}>
-                            <Button as={Link} to='register' basic size='large' color='orange' floated='right'
+                            <Button as={Link} to='register' size='large' color='orange' floated='right'
                                     className="home__button">
                                 Register
                             </Button>
                         </Grid.Column>
                         <Grid.Column width={8}>
-                            <Button as={Link} to='login' basic size='large' color='orange' floated='left'
+                            <Button as={Link} to='login' size='large' color='orange' floated='left'
                                     className="home__button">
                                 Login
                             </Button>
